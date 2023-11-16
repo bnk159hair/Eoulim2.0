1bcbcf6946df2bdde405353381a05b8549aca8c1
오영재 <dhdnqkrtk22@naver.com>
2023-11-16 11:04:44 +0900

    chore: add console log

--- front-end/src/components/stream/StreamCanvas.tsx
+++ front-end/src/components/stream/StreamCanvas.tsx
@@ -14,10 +14,20 @@ interface IProps {
 export const StreamCanvas = ({ streamManager, name, avatarPath, videoState }: IProps) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const { videoRef } = useStream(streamManager);
+  console.log(
+    '여기 videoRef:  !!!!',
+    videoRef.current?.clientWidth,
+    videoRef.current?.clientHeight,
+  );
   useFaceMask(videoRef.current, canvasRef.current, avatarPath);
 
   return (
     <>
+      {console.log(
+        '여기 canvasRef:  !!!!',
+        canvasRef.current?.clientWidth,
+        canvasRef.current?.clientHeight,
+      )}
       <Canvas ref={canvasRef} videoState={videoState} />
       <Video ref={videoRef} videoState={videoState} />
       <UserName>{name}</UserName>
